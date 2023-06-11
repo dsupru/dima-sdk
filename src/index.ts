23636258411aabf5c5b06fc2959817f70c087378
Dima S <dsuprunsw@gmail.com>
2023-06-11 07:02:48 -0700

    Add top-lvl index.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { LOTRRequest } from './ingress';
+import { Service } from './services';
+import { ApiResponse, ValidItems } from './types';
+import { LOTR } from './lotr';
+
+export { ApiResponse, LOTRRequest, Service, LOTR, ValidItems };
