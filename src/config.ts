a74d46650c8e41e49d494c7c464101747bed29ca
Dima S <dsuprunsw@gmail.com>
2023-06-11 06:26:01 -0700

    Add config.

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+import { LOTRConfig } from "./types";
+
+export function getConfig(): LOTRConfig {
+  return {
+    ingres: "prod",
+    services: [
+      {
+        name: "movie",
+        endpoint: "/movie",
+        uniqueField: "_id",
+        apiResponce: {
+          _id: "string",
+          name: "string",
+          runtimeInMinutes: "number",
+          budgetInMillions: "number",
+          boxOfficeRevenueInMillions: "number",
+          academyAwardNominations: "number",
+          academyAwardWins: "number",
+          rottenTomatesScore: "number"
+        }
+      },
+      {
+        name: "quote",
+        endpoint: "/quote",
+        uniqueField: "_id",
+        apiResponce: {
+          _id: "string",
+          dialog: "string",
+          movie: "string",
+          character: "string",
+          id: "string"
+        }
+      }
+    ]
+  }
+}
